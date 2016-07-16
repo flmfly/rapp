895238fc38a4e108f0d8b0b1ddc017e7aa51553c
Jeffrey <flmfly@gmail.com>
2016-07-16 14:16:07 +0800

    do much more again!

---
+++ app/pages/task-detail/task-detail.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the TaskDetailPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/task-detail/task-detail.html',
+})
+export class TaskDetailPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
